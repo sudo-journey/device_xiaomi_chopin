813c9913cdcbb8147378e43031bc9e87cad0d82a
wbs306 <wbs306@gmail.com>
2022-09-09 21:12:50 +0800

    chopin: Copy vibratorfeature file to blobs

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -45,5 +45,10 @@ write_headers "chopin"
 # The standard blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
+VIBRATOR_XML="vendor_overlay/31/etc/vintf/manifest/vendor.xiaomi.hardware.vibratorfeature.service.xml"
+printf '\n%s\n' "PRODUCT_COPY_FILES += \\" >> "$PRODUCTMK"
+printf '    %s/proprietary/%s:$(TARGET_COPY_OUT_PRODUCT)/%s\n' \
+                "$OUTDIR" "product/$VIBRATOR_XML" "$VIBRATOR_XML" >> "$PRODUCTMK"
+
 # Finish
 write_footers
